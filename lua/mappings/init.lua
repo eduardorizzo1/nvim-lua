f9a7934be9556a6a0e1c6d52f6a8415504434853
Eduardo Rizzo <eduardorizzo1@outlook.com>
2022-01-17 16:03:29 -0300

    add telescope mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -26,11 +26,20 @@ map('v', 'D', '"_d', opts)
 map('n', '<C-q>', ':q<CR>', opts)
 
 -- Toggle highlight match word
-map('n', '<space>*', ':noh', opts)
+map('n', '<leader>*', ':noh', opts)
 
 -- Lualine
 map('n', '<M-l>', ':bn <CR>', opts)
 map('n', '<M-h>', ':bp <CR>', opts)
 
 -- Prettier
-map('n', '<space>p', ':PrettierAsync<cr>', opts)  
+map('n', '<leader>p', ':PrettierAsync<cr>', opts)  
+
+-- Telescope
+map('n', '<c-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+map('n', ',ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+map('n', ',fg','<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+map('n', ',fb','<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
+map('n', ',fh','<cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
+map('n', ',gg','<cmd>lua require("telescope.builtin").git_status()<cr>', opts)
+
