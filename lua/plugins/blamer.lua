a34c740c005f219d85eb2f3ff5edc04a3a4561d3
Eduardo Rizzo <eduardorizzo1@outlook.com>
2022-04-10 00:48:28 -0300

    add blamer and fugitive

---
+++ lua/plugins/blamer.lua
@@ -0,0 +1,7 @@
+local g = vim.g
+
+g.blamer_enabled = 1
+g.blamer_delay = 0
+g.blamer_show_in_insert_modes = 0
+g.blamer_date_format = '%d/%m/%y'
+g.blamer_template = '<committer> • <author-time> • <summary>'
