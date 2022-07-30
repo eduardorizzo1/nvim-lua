27b835b35d100f66a5e8164275d25a417a5c1d32
Eduardo Rizzo <eduardorizzo1@outlook.com>
2022-07-29 23:14:57 -0300

    add evilline on lualine

--- lua/plugins/lsp/lsp-signature.lua
+++ lua/plugins/lsp/lsp-signature.lua
@@ -1,33 +1,33 @@
-cfg = {
-  debug = false, 
-  log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log",
-  verbose = false,
-  bind = true,
-  doc_lines = 10,
-  floating_window = true,
-  floating_window_above_cur_line = true,
-  fix_pos = false,
-  hint_enable = true,
-  hint_prefix = "🐼 ",
+local cfg = {
+	debug = false,
+	log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log",
+	verbose = false,
+	bind = true,
+	doc_lines = 10,
+	floating_window = true,
+	floating_window_above_cur_line = true,
+	fix_pos = false,
+	hint_enable = true,
+	hint_prefix = "🐼 ",
 	-- HINTS => 😏 💀 🐼 🐶 🐵 😜 ⭐ 👉 🪄
-  hint_scheme = "String",
-  use_lspsaga = false,
-  hi_parameter = "LspSignatureActiveParameter",
-  max_height = 12,
-  max_width = 120,
-  handler_opts = {
-    border = "rounded"
-  },
-  always_trigger = false,
-  auto_close_after = nil,
-  extra_trigger_chars = {},
-  zindex = 200,
-  padding = '',
-  transparency = nil,
-  shadow_blend = 36,
-  shadow_guibg = 'Black',
-  timer_interval = 200,
-  toggle_key = nil
+	hint_scheme = "String",
+	use_lspsaga = false,
+	hi_parameter = "LspSignatureActiveParameter",
+	max_height = 12,
+	max_width = 120,
+	handler_opts = {
+		border = "rounded",
+	},
+	always_trigger = false,
+	auto_close_after = nil,
+	extra_trigger_chars = {},
+	zindex = 200,
+	padding = "",
+	transparency = nil,
+	shadow_blend = 36,
+	shadow_guibg = "Black",
+	timer_interval = 200,
+	toggle_key = nil,
 }
 
-require('lsp_signature').setup(cfg)
+require("lsp_signature").setup(cfg)
