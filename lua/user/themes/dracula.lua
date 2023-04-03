a11ea0095aca68ea5342eed9326d38036ec00ab3
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-04-03 05:16:36 -0300

    change search hl

--- lua/user/themes/dracula.lua
+++ lua/user/themes/dracula.lua
@@ -6,7 +6,7 @@ local dracula = require("dracula")
 dracula.setup({
 	italic_comment = true,
 	overrides = {
-		Search = { fg = "#ffffff", bg = "#422fa3" },
+		Search = { fg = "#000000", bg = colors.green },
 		Title = { fg = colors.green },
 		Number = { fg = colors.purple },
 		Function = { fg = colors.green },
