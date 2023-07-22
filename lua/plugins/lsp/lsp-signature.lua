6495c5dded2fdce10f23211c0805ed4a49a7f895
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-07-22 10:55:00 -0300

    refact signature

--- lua/plugins/lsp/lsp-signature.lua
+++ lua/plugins/lsp/lsp-signature.lua
@@ -14,8 +14,7 @@ lsp_signature.setup({
 	fix_pos = false,
 	hint_enable = true,
 	hint_prefix = "🐼 ",
-	hint_scheme = "String",
-	use_lspsaga = false,
+	hint_scheme = "Keywords",
 	hi_parameter = "LspSignatureActiveParameter",
 	max_height = 12,
 	max_width = 120,
@@ -32,4 +31,25 @@ lsp_signature.setup({
 	shadow_guibg = "Black",
 	timer_interval = 200,
 	toggle_key = "<C-f>",
+	toggle_key_flip_floatwin_setting = true,
+	select_signature_key = "<M-f>",
+	move_cursor_key = nil,
+	floating_window_off_x = 5, -- adjust float windows x position.
+	floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
+		local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
+		local pumheight = vim.o.pumheight
+		local winline = vim.fn.winline() -- line number in the window
+		local winheight = vim.fn.winheight(0)
+
+		-- window top
+		if winline - 1 < pumheight then
+			return pumheight
+		end
+
+		-- window bottom
+		if winheight - winline < pumheight then
+			return -pumheight
+		end
+		return 0
+	end,
 })
