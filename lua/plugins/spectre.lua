dac8a6533d920479ab4ac9f94d2b1f7c1bd264c7
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-07-28 11:22:20 -0300

    add spectre configs

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -7,9 +7,9 @@ spectre.setup({
 	color_devicons = true,
 	open_cmd = "vnew",
 	live_update = false, -- auto execute search again when you write to any file in vim
-	line_sep_start = "┌-----------------------------------------",
-	result_padding = "¦  ",
-	line_sep = "└-----------------------------------------",
+	line_sep_start = "╭─────────────────────────────────────────",
+	result_padding = "│  ",
+	line_sep = "╰─────────────────────────────────────────",
 	highlight = {
 		ui = "String",
 		search = "DiffChange",
