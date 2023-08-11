146b62b7664a693135b23356fdc431f17a2036ac
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-08-10 21:33:36 -0300

    add new options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -28,11 +28,38 @@ local options = {
 	splitbelow = true,
 	showtabline = 2,
 	updatetime = 300,
-	completeopt = { "menuone", "noselect" },
+	completeopt = "menu,menuone,noselect",
 	foldlevel = 20,
 	foldmethod = "expr",
 	foldexpr = "nvim_treesitter#foldexpr()",
+	autowrite = true, -- Enable auto write
+	conceallevel = 3, -- Hide * markup for bold and italic
+	confirm = true, -- Confirm to save changes before exiting modified buffer
+	cursorline = true, -- Enable highlighting of the current line
+	expandtab = true, -- Use spaces instead of tabs
+	formatoptions = "jcroqlnt", -- tcqj
+	grepformat = "%f:%l:%c:%m",
+	grepprg = "rg --vimgrep",
+	ignorecase = true, -- Ignore case
+	inccommand = "nosplit", -- preview incremental substitute
+	laststatus = 0,
+	list = true, -- Show some invisible characters (tabs...
+	pumblend = 10, -- Popup blend
+	pumheight = 10, -- Maximum number of entries in a popup
+	scrolloff = 4, -- Lines of context
+	sessionoptions = { "buffers", "curdir", "tabpages", "winsize" },
+	shiftround = true, -- Round indent
+	showmode = false, -- Dont show mode since we have a statusline
+	sidescrolloff = 8, -- Columns of context
+	spelllang = { "en" },
+	timeoutlen = 300,
+	undolevels = 10000,
+	wildmode = "longest:full,full", -- Command-line completion mode
+	winminwidth = 5, -- Minimum window width
+	wrap = false, -- Disable line wrap
 }
+
+vim.opt.shortmess:append({ W = true, I = true, c = true })
 
 for key, value in pairs(options) do
 	vim.opt[key] = value
