4b76ac5f0e99e4fbe0667d6e6c852fca69c4f5bb
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-08-14 19:56:34 -0300

    lazy: add keymaps and opts

---
+++ lua/config/options.lua
@@ -0,0 +1,37 @@
+vim.api.nvim_exec("language en_US.UTF-8", true)
+
+local options = {
+	number = true,
+	relativenumber = false,
+	autoindent = true,
+	smartindent = true,
+	incsearch = true,
+	termguicolors = true,
+	mouse = "a",
+	mousemev = true,
+	background = "dark",
+	history = 5000,
+	clipboard = "unnamedplus",
+	encoding = "utf8",
+	tabstop = 2,
+	shiftwidth = 2,
+	smartcase = true,
+	hlsearch = true,
+	signcolumn = "yes",
+	swapfile = false,
+	undofile = true,
+	backup = false,
+	writebackup = false,
+	splitright = true,
+	splitbelow = true,
+	showtabline = 2,
+	updatetime = 300,
+	completeopt = "menu,menuone,noselect",
+	foldlevel = 20,
+	foldmethod = "expr",
+	foldexpr = "nvim_treesitter#foldexpr()",
+}
+
+for key, value in pairs(options) do
+	vim.opt[key] = value
+end
