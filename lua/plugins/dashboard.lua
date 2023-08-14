a0bb7318f35e2c6b0bb2c63c74ea416f4a51747c
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-08-14 19:47:36 -0300

    migrating from packer to lazy / add dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,33 +1,5 @@
 local icons = require("user.icons")
-local dashboard = require("dashboard")
-
 local custom_header = {
-	doom = {
-		"",
-		"",
-		"=================     ===============     ===============   ========  ========",
-		"\\\\ . . . . . . .\\\\   //. . . . . . .\\\\   //. . . . . . .\\\\  \\\\. . .\\\\// . . //",
-		"||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\\/ . . .||",
-		"|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||",
-		"||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||",
-		"|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\\ . . . . ||",
-		"||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\\_ . .|. .||",
-		"|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\\ `-_/| . ||",
-		"||_-' ||  .|/    || ||    \\|.  || `-_|| ||_-' ||  .|/    || ||   | \\  / |-_.||",
-		"||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \\  / |  `||",
-		"||    `'         || ||         `'    || ||    `'         || ||   | \\  / |   ||",
-		"||            .===' `===.         .==='.`===.         .===' /==. |  \\/  |   ||",
-		"||         .=='   \\_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \\/  |   ||",
-		"||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \\/  |   ||",
-		"||   .=='    _-'          `-__\\._-'         `-_./__-'         `' |. /|  |   ||",
-		"||.=='    _-'                                                     `' |  /==.||",
-		"=='    _-'                        N E O V I M                         \\/   `==",
-		"\\   _-'                                                                `-_   /",
-		" `''                                                                      ``'  ",
-		"",
-		"",
-	},
-	girl_neovim = {
 		"",
 		"",
 		" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
@@ -53,67 +25,68 @@ local custom_header = {
 		"│⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣│",
 		"╰──────────────────────────────╯",
 		"",
+}
+
+local opts = {
+	theme = "doom",
+	config = {
+		week_header = {
+			enable = false,
+		},
+		header = custom_header,
+		center = {
+			{
+				icon = icons.vim,
+				icon_hl = "Title",
+				desc = "neovim            ",
+				desc_hl = "String",
+				key = "a",
+				key_hl = "Number",
+				action = "e ~/.config/nvim",
+			},
+			{
+				icon = icons.terminal,
+				icon_hl = "Title",
+				desc = "kitty",
+				desc_hl = "String",
+				key = "b",
+				key_hl = "Number",
+				action = "e ~/.config/kitty",
+			},
+			{
+				icon = icons.file,
+				icon_hl = "Title",
+				desc = "dotfiles",
+				desc_hl = "String",
+				key = "c",
+				key_hl = "Number",
+				action = "e ~/dotfiles",
+			},
+			{
+				icon = icons.file,
+				icon_hl = "Title",
+				desc = "zsh",
+				desc_hl = "String",
+				key = "d",
+				key_hl = "Number",
+				action = "e ~/.zshrc",
+			},
+			{
+				icon = icons.file,
+				icon_hl = "Title",
+				desc = "p10k",
+				desc_hl = "String",
+				key = "e",
+				key_hl = "Number",
+				action = "e ~/.p10k.zsh",
+			},
+		},
 	},
 }
 
 return {
-	"glepnir/dashboard-nvim",
-	config = function()
-		dashboard.setup({
-			theme = "doom",
-			config = {
-				week_header = {
-					enable = false,
-				},
-				header = custom_header.girl_neovim,
-				center = {
-					{
-						icon = icons.vim,
-						icon_hl = "Title",
-						desc = "neovim            ",
-						desc_hl = "String",
-						key = "a",
-						key_hl = "Number",
-						action = "e ~/.config/nvim",
-					},
-					{
-						icon = icons.terminal,
-						icon_hl = "Title",
-						desc = "kitty",
-						desc_hl = "String",
-						key = "b",
-						key_hl = "Number",
-						action = "e ~/.config/kitty",
-					},
-					{
-						icon = icons.file,
-						icon_hl = "Title",
-						desc = "dotfiles",
-						desc_hl = "String",
-						key = "c",
-						key_hl = "Number",
-						action = "e ~/dotfiles",
-					},
-					{
-						icon = icons.file,
-						icon_hl = "Title",
-						desc = "zsh",
-						desc_hl = "String",
-						key = "d",
-						key_hl = "Number",
-						action = "e ~/.zshrc",
-					},
-					{
-						icon = icons.file,
-						icon_hl = "Title",
-						desc = "p10k",
-						desc_hl = "String",
-						key = "e",
-						key_hl = "Number",
-						action = "e ~/.p10k.zsh",
-					},
-				},
-			},
-		})
-	end,
+  "glepnir/dashboard-nvim",
+  config = function ()
+    require('dashboard').setup(opts)
+  end
 }
