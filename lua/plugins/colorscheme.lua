a57f47b9cb1902c923b32da117622030ae3ef19f
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-08-16 23:39:28 -0300

    lazy: add tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -121,4 +121,63 @@ local dracula = {
 	end
 }
 
+local tokyonight = {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  opts = {
+    style = "moon" ,
+	  transparent = false,
+	  styles = {
+	  	comments = { italic = false },
+	  	keywords = { italic = true },
+	  	functions = {},
+	  	variables = {},
+	  	sidebars = "dark",
+	  	floats = "dark",
+	  },
+
+	  on_highlights = function(hl, c)
+	  	local prompt = "#ff00ff"
+	  	hl.TelescopeNormal = {
+	  		bg = c.bg_dark,
+	  		fg = c.fg_dark,
+	  	}
+	  	hl.TelescopeBorder = {
+	  		bg = c.bg_dark,
+	  		fg = c.bg_dark,
+	  	}
+	  	hl.TelescopePromptNormal = {
+	  		bg = prompt,
+	  	}
+	  	hl.TelescopePromptBorder = {
+	  		bg = prompt,
+	  		fg = prompt,
+	  	}
+	  	hl.TelescopePromptTitle = {
+	  		bg = prompt,
+	  		fg = prompt,
+	  	}
+	  	hl.TelescopePreviewTitle = {
+	  		bg = c.bg_dark,
+	  		fg = c.bg_dark,
+	  	}
+	  	hl.TelescopeResultsTitle = {
+	  		bg = c.bg_dark,
+	  		fg = c.bg_dark,
+	  	}
+	  end,
+  },
+	init = function()
+		vim.cmd([[ colo tokyonight ]])
+		vim.cmd([[ hi NvimTreeVertSplit guifg=#1f2335 guibg=#1f2335 ]])
+		vim.cmd([[ hi NvimTreeFolderIcon guifg=#7aa2f7 gui=bold]])
+		vim.cmd([[ hi NvimTreeOpenedFolderName guifg=#bb9af7 gui=bold]])
+		vim.cmd([[ hi NvimTreeRootFolder guifg=#6272a4 gui=bold]])
+		vim.cmd([[ hi NvimTreeOpenedFile guifg=#73daca guibg=NONE gui=bold]])
+		vim.cmd([[ hi IndentBlanklineContextStart guifg=#BB9AF7 gui=none]])
+		vim.cmd([[ hi IndentBlanklineContextChar guifg=#BB9AF7 gui=none]])
+		vim.cmd([[ hi IndentBlanklineIndent1 guifg=#333952 gui=nocombine]])
+	end
+}
+
 return dracula
