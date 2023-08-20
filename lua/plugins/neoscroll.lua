63afa8ec812a40b223531c5358eba8010fefeae4
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-08-20 01:15:47 -0300

    lazy: add trouble and neoscroll

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,14 @@
+return {
+  "karb94/neoscroll.nvim",
+  opts = {
+    mappings = { "<C-u>", "<C-d>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+	  hide_cursor = true,
+	  stop_eof = true, 
+	  use_local_scrolloff = false,
+	  respect_scrolloff = false,
+	  cursor_scrolls_alone = true,
+	  easing_function = nil,
+	  pre_hook = nil,
+	  post_hook = nil,
+  }
+}
