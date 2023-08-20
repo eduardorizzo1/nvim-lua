c2c21670bcd556ea0320b837d1bbdd0871193ecb
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-08-20 01:32:49 -0300

    lazy: add todo and comment

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,23 @@
+return {
+  "numToStr/Comment.nvim",
+  opts = {
+    padding = true,
+  	sticky = true,
+  	ignore = nil,
+  	toggler = {
+  		line = "<leader>cc",
+  		block = "<leader>ca",
+  	},
+  	opleader = {
+  		line = "<leader>cc",
+  		block = "<leader>ca",
+  	},
+  	mappings = {
+  		basic = true,
+  		extra = true,
+  		extended = false,
+  	},
+  	pre_hook = nil,
+  	post_hook = nil,
+  }
+}
