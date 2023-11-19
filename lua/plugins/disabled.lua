ac9f55340b176f45ec1bfcbda83218a222c18809
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-11-19 18:26:23 -0300

    remove noice/notify/flash

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,7 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
