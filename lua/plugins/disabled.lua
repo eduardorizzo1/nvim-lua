dbaf820c42fb7c9afe467a3cd74cbf41c1c97035
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-11-19 19:17:59 -0300

    add dracula

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
 }
