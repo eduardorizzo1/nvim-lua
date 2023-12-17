787af9593c4ba3a0a942eb082d2ba12954b1d5e8
Eduardo Rizzo <eduardorizzo1@outlook.com>
2023-12-17 09:24:20 -0300

    update plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,6 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
 }
