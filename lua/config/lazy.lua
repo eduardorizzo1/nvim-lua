ec33897f3c77492857342ca19840b729a846dc5e
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-01-30 08:58:38 -0300

    more lazy configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,23 @@ if not vim.g.vscode then
 		ui = {
 			border = "rounded",
 		},
+		install = { colorscheme = { "tokyonight", "habamax" } },
+		checker = { enabled = true }, -- automatically check for plugin updates
+		performance = {
+			rtp = {
+				-- disable some rtp plugins
+				disabled_plugins = {
+					"gzip",
+					-- "matchit",
+					-- "matchparen",
+					-- "netrwPlugin",
+					"tarPlugin",
+					"tohtml",
+					"tutor",
+					"zipPlugin",
+				},
+			},
+		},
 	})
 else
 	require("lazy").setup({
