277a873b62c0ea289f6141ef4e07fb586c8ec74d
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-02-10 17:49:17 -0300

    add neodev

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,9 @@
 return {
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "folke/flash.nvim", enabled = false },
-	{ "folke/neodev.nvim", enabled = false },
-	{ "nvim-treesitter/nvim-treesitter-context", enabled = false },
-	{ "Exafunction/codeium.nvim", enabled = false },
-	{ "echasnovski/mini.pairs", enabled = false },
-	{ "stevearc/conform.nvim", enabled = false },
-	{ "mfussenegger/nvim-lint", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim",             enabled = false },
+  { "folke/flash.nvim",                        enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  { "Exafunction/codeium.nvim",                enabled = false },
+  { "echasnovski/mini.pairs",                  enabled = false },
+  { "stevearc/conform.nvim",                   enabled = false },
+  { "mfussenegger/nvim-lint",                  enabled = false },
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,67 +1,63 @@
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-			cmdline = {
-				enabled = true,
-				view = "cmdline_popup",
-				opts = {},
-			},
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
-			lsp = {
-				notify = {
-					enabled = false,
-				},
-				signature = {
-					enabled = false,
-				},
-				progress = {
-					enabled = false,
-				},
-				hover = {
-					enabled = false,
-					silent = true,
-					view = nil,
-				},
-			},
-			health = {
-				checker = false,
-			},
-			messages = {
-				-- NOTE: If you enable messages, then the cmdline is enabled automatically.
-				-- This is a current Neovim limitation. it
-				-- false | virtual-text | messages or notify
-				enabled = true, -- enables the Noice messages UI
-				view = "notify", -- default view for messages
-				view_error = "notify", -- view for errors
-				view_warn = "notify", -- view for warnings
-				view_history = "messages", -- view for :messages
-				view_search = false,
-			},
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			-- "rcarriga/nvim-notify",
-		},
-	},
-	{
-		"folke/noice.nvim",
-		opts = function(_, opts)
-			table.insert(opts.routes, {
-				filter = {
-					event = "notify",
-					find = "No information available",
-				},
-				opts = { skip = true },
-			})
-		end,
-	},
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+        opts = {},
+      },
+      presets = {
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,        -- add a border to hover docs and signature help
+      },
+      lsp = {
+        notify = {
+          enabled = false,
+        },
+        signature = {
+          enabled = true,
+        },
+        progress = {
+          enabled = false,
+        },
+        hover = {
+          enabled = false,
+          silent = true,
+          view = nil,
+        },
+      },
+      health = {
+        checker = false,
+      },
+      messages = {
+        enabled = true,            -- enables the Noice messages UI
+        view = "notify",           -- default view for messages
+        view_error = "notify",     -- view for errors
+        view_warn = "notify",      -- view for warnings
+        view_history = "messages", -- view for :messages
+        view_search = false,       -- false | virtual-text | messages or notify
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+    end,
+  },
 }
