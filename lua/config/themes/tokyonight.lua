0fd8cd7c3b810d7ae5e08c7c5302b61375136814
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-02-10 21:17:49 -0300

    fix imports tokyonight

--- lua/config/themes/tokyonight.lua
+++ lua/config/themes/tokyonight.lua
@@ -5,87 +5,88 @@ local custom_bg = "#1E2030"
 local custom_fg = "#2f334d"
 
 M.opts = {
-	style = "moon",
-	transparent = transparent,
-	styles = {
-		comments = { italic = true },
-		keywords = { italic = true },
-		functions = {},
-		variables = {},
-		sidebars = transparent and "transparent" or "dark",
-		floats = transparent and "transparent" or "dark",
-	},
+  style = "moon",
+  transparent = transparent,
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    sidebars = transparent and "transparent" or "dark",
+    floats = transparent and "transparent" or "dark",
+  },
 
-	on_highlights = function(hl, c)
-		hl.CursorLineNr = {
-			fg = c.cyan,
-		}
-		hl.WinSeparator = { fg = c.comment }
+  on_highlights = function(hl, c)
+    hl.CursorLineNr = {
+      fg = c.cyan,
+    }
+    hl.WinSeparator = { fg = c.comment }
+    hl.Include = { fg = c.cyan, italic = true }
 
-		--HACK:======== NvimTree ========
-		hl.NvimTreeOpenedFolderName = {
-			bg = c.bg_highlight,
-			fg = c.magenta,
-		}
-		hl.NvimTreeOpenedFile = {
-			bg = c.none,
-			fg = c.cyan,
-		}
-		hl.NvimTreeWinSeparator = {
-			bg = c.bg,
-			fg = c.comment,
-		}
+    --HACK:======== NvimTree ========
+    hl.NvimTreeOpenedFolderName = {
+      bg = c.bg_highlight,
+      fg = c.magenta,
+    }
+    hl.NvimTreeOpenedFile = {
+      bg = c.none,
+      fg = c.cyan,
+    }
+    hl.NvimTreeWinSeparator = {
+      bg = c.bg,
+      fg = c.comment,
+    }
 
-		--HACK:======== Telescope ========
-		hl.TelescopeBorder = {
-			bg = transparent and c.none or c.bg_dark,
-			fg = c.blue,
-		}
-		hl.TelescopePromptCounter = {
-			bg = c.bg_dark,
-			fg = c.cyan,
-		}
-		hl.TelescopeMatching = { fg = c.blue, bold = true }
-		hl.TelescopeSelection = { bg = c.bg_highlight, bold = true }
+    --HACK:======== Telescope ========
+    hl.TelescopeBorder = {
+      bg = transparent and c.none or c.bg_dark,
+      fg = c.blue,
+    }
+    hl.TelescopePromptCounter = {
+      bg = c.bg_dark,
+      fg = c.cyan,
+    }
+    hl.TelescopeMatching = { fg = c.blue, bold = true }
+    hl.TelescopeSelection = { bg = c.bg_highlight, bold = true }
 
-		--HACK:======== Bufferline ========
-		hl.BufferLineFill = transparent and {
-			bg = custom_bg,
-		} or {}
-		hl.BufferLineBackground = transparent and {
-			fg = custom_fg,
-		} or {}
-		hl.BufferLineOffsetSeparator = {
-			bg = c.bg,
-			fg = c.bg_dark,
-		}
-		hl.BufferLineSeparator = transparent and {
-			fg = custom_bg,
-		} or {}
-		hl.BufferLineSeparatorSelected = transparent and {
-			fg = custom_bg,
-		} or {}
-		hl.BufferLineSeparatorVisible = transparent and {
-			fg = custom_bg,
-		} or {}
-		hl.BufferLineBufferVisible = { fg = c.fg }
-		hl.BufferLineBufferSelected = { fg = c.fg, italic = true, bold = true }
-		hl.BufferLineTab = transparent and { fg = c.comment, bg = custom_bg } or {}
-		hl.BufferLineTabSeparator = transparent and { fg = custom_bg, bg = custom_bg } or {}
-		hl.BufferLineTabSeparatorSelected = transparent and { fg = custom_bg } or {}
+    --HACK:======== Bufferline ========
+    hl.BufferLineFill = transparent and {
+      bg = custom_bg,
+    } or {}
+    hl.BufferLineBackground = transparent and {
+      fg = custom_fg,
+    } or {}
+    hl.BufferLineOffsetSeparator = {
+      bg = c.bg,
+      fg = c.bg_dark,
+    }
+    hl.BufferLineSeparator = transparent and {
+      fg = custom_bg,
+    } or {}
+    hl.BufferLineSeparatorSelected = transparent and {
+      fg = custom_bg,
+    } or {}
+    hl.BufferLineSeparatorVisible = transparent and {
+      fg = custom_bg,
+    } or {}
+    hl.BufferLineBufferVisible = { fg = c.fg }
+    hl.BufferLineBufferSelected = { fg = c.fg, italic = true, bold = true }
+    hl.BufferLineTab = transparent and { fg = c.comment, bg = custom_bg } or {}
+    hl.BufferLineTabSeparator = transparent and { fg = custom_bg, bg = custom_bg } or {}
+    hl.BufferLineTabSeparatorSelected = transparent and { fg = custom_bg } or {}
 
-		--HACK:========= Cmp =========
-		hl.Pmenu = transparent and {
-			bg = c.none,
-		} or {}
+    --HACK:========= Cmp =========
+    hl.Pmenu = transparent and {
+      bg = c.none,
+    } or {}
 
-		--HACK:======== Fidget ========
-		hl.FidgetTask = { fg = c.comment, italic = true }
-		hl.FidgetTitle = { fg = c.cyan, italic = true, bold = true }
+    --HACK:======== Fidget ========
+    hl.FidgetTask = { fg = c.comment, italic = true }
+    hl.FidgetTitle = { fg = c.cyan, italic = true, bold = true }
 
-		--HACK:====== Typescript =======
-		hl.typescriptImport = { italic = true }
-	end,
+    --HACK:====== Typescript =======
+    hl.typescriptImport = { italic = true }
+  end,
 }
 
 return M
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,15 +1,15 @@
 return {
-	"rcarriga/nvim-notify",
-	opts = {
-		timeout = 2000,
-		max_height = function()
-			return math.floor(vim.o.lines * 0.35)
-		end,
-		max_width = function()
-			return math.floor(vim.o.columns * 0.35)
-		end,
-		on_open = function(win)
-			vim.api.nvim_win_set_config(win, { zindex = 100 })
-		end,
-	},
+  "rcarriga/nvim-notify",
+  opts = {
+    timeout = 2000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.5)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.5)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  },
 }
