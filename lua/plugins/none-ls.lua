babb930c71b848aa04e69e95456663283383e609
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-02-27 14:50:38 -0300

    add csharpier none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,23 +9,7 @@ return {
 			nls.builtins.formatting.shfmt,
 			nls.builtins.formatting.prettierd,
 			nls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-			-- nls.builtins.formatting.jq,
-			-- nls.builtins.formatting.rustfmt.with({
-			--   extra_args = function(params)
-			--     local Path = require("plenary.path")
-			--     local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
-			--
-			--     if cargo_toml:exists() and cargo_toml:is_file() then
-			--       for _, line in ipairs(cargo_toml:readlines()) do
-			--         local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
-			--         if edition then
-			--           return { "--edition=" .. edition }
-			--         end
-			--       end
-			--     end
-			--     return { "--edition=2021" }
-			--   end,
-			-- }),
+			nls.builtins.formatting.csharpier,
 		})
 	end,
 }
