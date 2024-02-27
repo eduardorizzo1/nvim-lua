0af366dc0602bd282985df6d7550147be60d6e5b
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-02-27 19:09:10 -0300

    refact cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,162 +1,162 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-cmdline",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
-    {
-      "tzachar/cmp-tabnine",
-      build = "./install.sh",
-      dependencies = "hrsh7th/nvim-cmp",
-    },
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		{
+			"tzachar/cmp-tabnine",
+			build = "./install.sh",
+			dependencies = "hrsh7th/nvim-cmp",
+		},
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/utils/snippets" } })
+		require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/utils/snippets" } })
 
-    local check_backspace = function()
-      local col = vim.fn.col(".") - 1
-      return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-    end
+		local check_backspace = function()
+			local col = vim.fn.col(".") - 1
+			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+		end
 
-    local icons = require("utils.icons").kinds
+		local icons = require("utils.icons").kinds
 
-    local custom_icons = {
-      cmp_tabnine = "⚡",
-      luasnip = " ",
-      buffer = "◉ ",
-    }
+		local custom_icons = {
+			cmp_tabnine = "⚡",
+			luasnip = " ",
+			buffer = "◉ ",
+		}
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
 
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+				["<C-e>"] = cmp.mapping({
+					i = cmp.mapping.abort(),
+					c = cmp.mapping.close(),
+				}),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expandable() then
+						luasnip.expand()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif check_backspace() then
+						fallback()
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-      }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+			}),
 
-      formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          -- local kind_name = string.lower(string.sub(vim_item.kind, 1, 4))
-          local kind_name = vim_item.kind
-          local space = "     "
-          vim_item.kind = string.format("%s", icons[vim_item.kind])
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					local kind_name = vim_item.kind
+					local space = "     "
+					vim_item.kind = string.format("%s", icons[vim_item.kind])
 
-          for name, icon in pairs(custom_icons) do
-            if entry.source.name == name then
-              vim_item.kind = icon
-            end
-          end
+					for name, icon in pairs(custom_icons) do
+						if entry.source.name == name then
+							vim_item.kind = icon
+						end
+					end
 
-          --NOTE: order matters
-          vim_item.menu = ({
-            nvim_lsp = space .. "❨" .. kind_name .. "❩",
-            cmp_tabnine = space .. "❨Tabnine❩",
-            luasnip = space .. "❨Luasnip❩",
-            buffer = space .. "❨Buffer❩",
-            nvim_lua = space .. "❨Nvim❩",
-          })[entry.source.name]
-          return vim_item
-        end,
-      },
+					--NOTE: order matters
+					vim_item.menu = ({
+						nvim_lsp = space .. "❨" .. kind_name .. "❩",
+						cmp_tabnine = space .. "❨Tabnine❩",
+						luasnip = space .. "❨Luasnip❩",
+						buffer = space .. "❨Buffer❩",
+						nvim_lua = space .. "❨Nvim❩",
+					})[entry.source.name]
+					return vim_item
+				end,
+				expandable_indicator = true,
+			},
 
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "cmp_tabnine" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "nvim_lua" },
-      },
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "cmp_tabnine" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "nvim_lua" },
+			},
 
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
+			confirm_opts = {
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = false,
+			},
 
-      window = {
-        documentation = {
-          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-          winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-        },
-        completion = {
-          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-          winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-        },
-      },
+			window = {
+				documentation = {
+					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+				},
+				completion = {
+					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+				},
+			},
 
-      experimental = {
-        ghost_text = false,
-      },
-    })
+			experimental = {
+				ghost_text = false,
+			},
+		})
 
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
 
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
-  end,
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+	end,
 }
