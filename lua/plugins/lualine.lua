cad84ab752f86b26a318bfa4739fd016254ce1f0
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-02-27 14:50:11 -0300

    refact lualine configs

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,51 +1,3 @@
-local icons = require("utils.icons")
-local colors = require("utils.colors").color
-local mode_color = require("utils.colors").mode_color
-local dracula = false
-
-local conditions = {
-	buffer_not_empty = function()
-		return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
-	end,
-	hide_in_width = function()
-		return vim.fn.winwidth(0) > 80
-	end,
-	check_git_workspace = function()
-		local filepath = vim.fn.expand("%:p:h")
-		local gitdir = vim.fn.finddir(".git", filepath .. ";")
-		return gitdir and #gitdir > 0 and #gitdir < #filepath
-	end,
-}
-
--- ======================================================
-local color_bg_mode = function()
-	return dracula and { bg = mode_color[vim.fn.mode()], fg = colors.bg_dark } or {}
-end
-
-local color_fg_mode = function()
-	return dracula and { bg = colors.bg_alt, fg = mode_color[vim.fn.mode()] } or {
-		fg = mode_color[vim.fn.mode()],
-	}
-end
-
---PERF: =========== Mode ==============
-local mode = {
-	{
-		function()
-			return " "
-		end,
-		color = color_bg_mode,
-		padding = { left = 1, right = 0 },
-	},
-	{
-		"mode",
-		color = color_bg_mode,
-		separator = { right = "" },
-		padding = { left = 0, right = 1 },
-	},
-}
-
---PERF: =========== Server ==============
 local server = function()
 	local msg = " Lsp"
 	local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
@@ -62,186 +14,27 @@ local server = function()
 	return msg
 end
 
---PERF: =========== Branch ==============
-local branch = {
-	"branch",
-	separator = { right = "" },
-	color = dracula and color_fg_mode or {},
-	padding = { left = 1, right = 1 },
-}
-
---PERF: =========== Noice ==============
-local noice = {
-	function()
-		return require("noice").api.status.command.get()
-		-- return "󰥻 " .. require("noice").api.status.command.get()
-	end,
-	cond = function()
-		return package.loaded["noice"] and require("noice").api.status.command.has()
-	end,
-	color = dracula and { fg = colors.pink } or { fg = "#BB9AF7" },
-}
-
---PERF: ========= Left Arrow ===========
-local left_arrow = {
-	function()
-		return icons.arrows.left
-	end,
-	color = dracula and { fg = colors.comment } or {},
-	padding = { right = 0, left = 0 },
-}
-
---PERF: ========= Right Arrow ===========
-local right_arrow = {
-	function()
-		return icons.arrows.right
-	end,
-	color = dracula and { fg = colors.comment } or {},
-	padding = { right = 0, left = 0 },
-}
-
---HACK: =========== DEFAULT =============
-local default = {
-	options = {
-		icons_enabled = true,
-		theme = dracula and {
-			normal = {
-				a = { bg = colors.bg },
-				b = { bg = colors.bg },
-				c = { bg = colors.bg },
-				x = { bg = colors.bg },
-				y = { bg = colors.bg },
-				z = { bg = colors.bg },
-			},
-		} or "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "dashboard" },
-		ignore_focus = {},
-		always_divide_middle = true,
-		globalstatus = true,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-	},
-	sections = {
-		lualine_a = mode,
-		lualine_b = {
-			branch,
-		},
-		lualine_c = {
-			{
-				"diagnostics",
-				sources = { "nvim_diagnostic" },
-				gui = "bold",
-				padding = { right = 1, left = 1 },
-			},
-			-- right_arrow,
-			{
-				"filetype",
-				icon_only = true,
-				colored = true,
-				separator = { left = "", right = "" },
-				padding = { left = 1, right = 0 },
-			},
-			{
-				"filename",
-				cond = conditions.buffer_not_empty,
-				color = dracula and { fg = colors.fg } or {},
-				shorten = true,
-				file_status = false,
-				padding = { left = 1, right = 1 },
-			},
-		},
-		lualine_x = {
-			noice,
-			{
-				"diff",
-				symbols = {
-					added = icons.git_status.added,
-					modified = icons.git_status.modified,
-					removed = icons.git_status.removed,
+return {
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		sections = {
+			lualine_a = {
+				{
+					"mode",
+					icon = "",
 				},
 			},
-			{
-				"searchcount",
-				color = dracula and { fg = colors.yellow } or { fg = "#E0AF68" },
+			lualine_z = {
+				{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+				{ "location", padding = { left = 0, right = 1 } },
+			},
+			lualine_y = {
+				{
+					server,
+					separator = { left = "" },
+					padding = { right = 1, left = 1 },
+				},
 			},
 		},
-		lualine_y = {
-			{
-				server,
-				color = dracula and color_fg_mode or {},
-				separator = { left = "" },
-				padding = { right = 1, left = 1 },
-			},
-		},
-		lualine_z = {
-			{
-				"progress",
-				color = color_bg_mode,
-				padding = { right = 0, left = 1 },
-				separator = { left = "" },
-			},
-			{
-				"location",
-				color = color_bg_mode,
-				padding = { right = 1, left = 0 },
-			},
-		},
-	},
-}
-
----------------------------------------------------------
----------------------------------------------------------
----------------------------------------------------------
----------------------------------------------------------
-
-return {
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = default,
-		-- opts = require("utils.bubbles"),
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		optional = true,
-		event = "VeryLazy",
-		opts = function(_, opts)
-			table.insert(opts.sections.lualine_x, 4, {
-				function()
-					return icons.arrows.left
-				end,
-				cond = require("lazy.status").has_updates,
-				color = dracula and { fg = colors.comment } or {},
-				padding = { right = 0, left = 0 },
-			})
-			table.insert(opts.sections.lualine_x, 5, {
-				require("lazy.status").updates,
-				cond = require("lazy.status").has_updates,
-				color = dracula and { fg = colors.cyan } or { fg = "#7DCFFF" },
-				padding = { right = 1, left = 1 },
-			})
-		end,
-		-- table.insert(opts.sections.lualine_x, 1, {
-		--   function()
-		--     return icons.arrows.left
-		--   end,
-		--   cond = function()
-		--     return package.loaded["dap"] and require("dap").status() ~= ""
-		--   end,
-		--   color = dracula and { fg = colors.comment } or {},
-		--   padding = { right = 0, left = 0 },
-		-- })
-		-- table.insert(opts.sections.lualine_x, 2, {
-		--   function()
-		--     return "  " .. require("dap").status()
-		--   end,
-		--   cond = function()
-		--     return package.loaded["dap"] and require("dap").status() ~= ""
-		--   end,
-		-- })
 	},
 }
