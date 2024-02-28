dfd3e304303cd9e8bd4bb1526c6caedd88e15975
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-02-28 10:56:13 -0300

    add diffview.nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,7 +36,6 @@ if not vim.g.vscode then
 			{ import = "lazyvim.plugins.extras.editor.leap" },
 			{ import = "lazyvim.plugins.extras.util.dot" },
 			{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-			{ import = "lazyvim.plugins.extras.ui.edgy" },
 			{ import = "lazyvim.plugins.extras.coding.codeium" },
 			{ import = "plugins" },
 		},
