e12e34524321e0a46eea9869d395d33e1cc26e79
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-03-08 15:24:38 -0300

    remove codelens keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,8 @@ return {
 		local keys = require("lazyvim.plugins.lsp.keymaps").get()
 		keys[#keys + 1] = { "K", false }
 		keys[#keys + 1] = { "gr", false }
+		keys[#keys + 1] = { "<leader>cc", false } -- Run Codelens
+		keys[#keys + 1] = { "<leader>cC", false } -- Refresh & Display Codelens
 		vim.diagnostic.config({
 			virtual_text = require("utils.virtual-text").virtual_text,
 		})
