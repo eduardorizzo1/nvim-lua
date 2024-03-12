c389ba9ccd9bbd0d3fa4bf424e2c84abf07bbf06
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-03-12 10:31:04 -0300

    refact noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,15 +3,7 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
-			cmdline = {
-				enabled = true,
-				view = "cmdline_popup",
-				opts = {},
-			},
 			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = true, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = true, -- add a border to hover docs and signature help
 			},
@@ -31,9 +23,6 @@ return {
 					view = nil,
 				},
 			},
-			health = {
-				checker = true,
-			},
 			messages = {
 				enabled = true, -- enables the Noice messages UI
 				view = "notify", -- default view for messages
@@ -42,10 +31,6 @@ return {
 				view_history = "messages", -- view for :messages
 				view_search = false, -- false | virtual-text | messages or notify
 			},
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
 		},
 	},
 	{
