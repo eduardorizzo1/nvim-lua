30b6e43aa641209260703720cb88d9b2734df112
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-03-12 19:20:57 -0300

    refact autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -11,18 +11,6 @@ return {
 				lua = { "string", "source" },
 				javascript = { "string", "template_string" },
 			},
-			disable_filetype = { "TelescopePrompt", "spectre_panel" },
-			fast_wrap = {
-				map = "<M-e>",
-				chars = { "{", "[", "(", '"', "'" },
-				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-				offset = 0,
-				end_key = "$",
-				keys = "qwertyuiopzxcvbnmasdfghjkl",
-				check_comma = true,
-				highlight = "PmenuSel",
-				highlight_grey = "LineNr",
-			},
 		})
 
 		npairs.add_rules({
