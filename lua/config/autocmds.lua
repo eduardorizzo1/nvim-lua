7b9a4c0cd756b443a0ed7b1823afd51002accdc8
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-03-27 22:59:22 -0300

    add autocmd for prevent o/O comment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,12 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+vim.api.nvim_create_augroup("discontinue_comments", { clear = true })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions - "o"
+	end,
+	group = "discontinue_comments",
+	desc = "Dont't continue comments with o/O",
+})
 
 -- vim.api.nvim_create_autocmd("LspAttach", {
 -- 	callback = function(args)
