f7cbc58af5f9469e0eebe47d6efeee43fbd80507
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-06-10 17:34:00 +1000

    remove ts inlayhints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,4 +12,22 @@ return {
 			codelens = false,
 		})
 	end,
+	opts = {
+		servers = {
+			vtsls = {
+				settings = {
+					typescript = {
+						inlayHints = {
+							enumMemberValues = { enabled = false },
+							functionLikeReturnTypes = { enabled = false },
+							parameterNames = { enabled = false },
+							parameterTypes = { enabled = false },
+							propertyDeclarationTypes = { enabled = false },
+							variableTypes = { enabled = false },
+						},
+					},
+				},
+			},
+		},
+	},
 }
