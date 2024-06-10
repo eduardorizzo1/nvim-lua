0ffed734985414f348aed39c6a405f16125e009d
Eduardo Rizzo <eduardorizzo1@outlook.com>
2024-06-10 17:34:10 +1000

    remove navic from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -46,6 +46,7 @@ return {
 		"nvim-lualine/lualine.nvim",
 		opts = function(_, opts)
 			table.remove(opts.sections.lualine_x, 2)
+			table.remove(opts.sections.lualine_c)
 			table.insert(opts.sections.lualine_x, 2, {
 				"searchcount",
 			})
