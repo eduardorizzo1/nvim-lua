4ba80cc17b481e95b9ec5d71554da3603ef60f3b
Eduardo Rizzo <eduardorizzo1@outlook.com>
2025-05-15 12:53:00 +1000

    update with snacks plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,27 +1,160 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
-		dashboard = {
-			enabled = false,
-		},
-
 		indent = {
 			enabled = true,
-
 			indent = {
-				enabled = true,
 				char = "▏",
 			},
-
 			scope = {
-				enabled = true,
 				char = "▎",
 			},
 			animate = {
+				style = "out",
+				easing = "linear",
 				duration = {
-					total = 2500,
+					step = 20,
+					total = 5000,
 				},
 			},
 		},
+
+		picker = {
+			win = {
+				input = {
+					keys = {
+						["<CR>"] = { "close_explorer", mode = { "i", "n" } },
+					},
+					list = {
+						keys = {
+							["<CR>"] = "close_explorer",
+						},
+					},
+				},
+			},
+			actions = {
+				close_explorer = function(picker)
+					---@diagnostic disable-next-line: undefined-global
+					local is_open = vim.inspect(Snacks.picker.get({ source = "explorer" }))
+
+					if is_open ~= "{}" or is_open == 0 then
+						vim.cmd("lua Snacks.explorer.open()")
+					end
+
+					picker:action("confirm")
+				end,
+			},
+			sources = {
+				explorer = {
+					actions = {
+						toggle_dir = function(picker, item)
+							if item.dir == false then
+								picker:action("confirm")
+								picker:action("close")
+							else
+								picker:action("confirm")
+							end
+						end,
+					},
+					win = {
+						list = {
+							keys = {
+								["<c-p>"] = false,
+								["<c-k>"] = false,
+								["<c-j>"] = false,
+								["<c-b>"] = false,
+								["l"] = false,
+								["h"] = false,
+								["<S-l>"] = false,
+								["<cr>"] = "toggle_dir",
+								["<leader>fg"] = false,
+							},
+						},
+					},
+					layout = {
+						auto_hide = { "input" },
+					},
+				},
+			},
+		},
+
+		dashboard = {
+			enabled = true,
+			width = 50,
+			preset = {
+				header = [[
+	           ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗
+	           ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║
+	           ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║
+	           ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║
+	           ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║
+	           ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝
+	          ╭──────────────────────────────╮
+	          │⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ │
+	          │⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇│
+	          │⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽│
+	          │⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕│
+	          │⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕│
+	          │⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕│
+	          │⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄│
+	          │⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕│
+	          │⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿│
+	          │⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿│
+	          │⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟│
+	          │⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠│
+	          │⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙│
+	          │⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣│
+	          ╰──────────────────────────────╯
+        ]],
+				keys = {
+					{
+						icon = " ",
+						key = "a",
+						desc = "Neovim",
+						action = ":cd ~/.config/nvim | :lua Snacks.explorer.open()",
+					},
+					{
+						icon = " ",
+						key = "b",
+						desc = "Kitty",
+						action = ":cd ~/.config/kitty | :lua Snacks.explorer.open()",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Dotfiles",
+						action = ":cd ~/dotfiles | :lua Snacks.explorer.open()",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = "󰒲 ",
+						key = "l",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+			},
+		},
+	},
+
+	keys = {
+		{ "<leader>fg", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Live Grep (Root Dir)" },
+		{
+			"<leader>fG",
+			function()
+				---@diagnostic disable-next-line: undefined-global
+				Snacks.picker.git_files()
+			end,
+			desc = "Find Files (git-files)",
+		},
+		{ "<C-p>", "<cmd>lua Snacks.picker.files()<cr>", desc = "Find Files (Root Dir)" },
+		{ "<leader><space>", false },
 	},
 }
