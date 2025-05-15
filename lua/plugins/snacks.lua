2fe5ea3404882b526a71c140181a5b7b11c65e5b
Eduardo Rizzo <eduardorizzo1@outlook.com>
2025-05-15 12:36:00 +1000

    add explorer and picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,83 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
+		indent = {
+			enabled = true,
+			indent = {
+				char = "▏",
+			},
+			scope = {
+				char = "▎",
+			},
+			animate = {
+				style = "out",
+				easing = "linear",
+				duration = {
+					step = 20,
+					total = 5000,
+				},
+			},
+		},
+
+		picker = {
+			win = {
+				input = {
+					keys = {
+						["<CR>"] = { "close_explorer", mode = { "i", "n" } },
+					},
+					list = {
+						keys = {
+							["<CR>"] = "close_explorer",
+						},
+					},
+				},
+			},
+			actions = {
+				close_explorer = function(picker)
+					---@diagnostic disable-next-line: undefined-global
+					local is_open = vim.inspect(Snacks.picker.get({ source = "explorer" }))
+
+					if is_open ~= "{}" or is_open == 0 then
+						vim.cmd("lua Snacks.explorer.open()")
+					end
+
+					picker:action("confirm")
+				end,
+			},
+			sources = {
+				explorer = {
+					actions = {
+						toggle_dir = function(picker, item)
+							if item.dir == false then
+								picker:action("confirm")
+								picker:action("close")
+							else
+								picker:action("confirm")
+							end
+						end,
+					},
+					win = {
+						list = {
+							keys = {
+								["<c-p>"] = false,
+								["<c-k>"] = false,
+								["<c-j>"] = false,
+								["<c-b>"] = false,
+								["l"] = false,
+								["h"] = false,
+								["<S-l>"] = false,
+								["<cr>"] = "toggle_dir",
+								["<leader>fg"] = false,
+							},
+						},
+					},
+					layout = {
+						auto_hide = { "input" },
+					},
+				},
+			},
+		},
+
 		dashboard = {
 			enabled = true,
 			width = 50,
@@ -65,56 +142,19 @@ return {
 				},
 			},
 		},
+	},
 
-		indent = {
-			indent = {
-				char = "▏",
-			},
-			scope = {
-				char = "▎",
-			},
-			animate = {
-				style = "out",
-				easing = "linear",
-				duration = {
-					step = 20,
-					total = 5000,
-				},
-			},
+	keys = {
+		{ "<leader>fg", "<cmd>lua Snacks.picker.grep()<cr>", desc = "Live Grep (Root Dir)" },
+		{
+			"<leader>fG",
+			function()
+				---@diagnostic disable-next-line: undefined-global
+				Snacks.picker.git_files()
+			end,
+			desc = "Find Files (git-files)",
 		},
-
-		picker = {
-			sources = {
-				explorer = {
-					actions = {
-						toggle_dir = function(picker, item)
-							if item.dir == false then
-								picker:action("confirm")
-								picker:action("close")
-							else
-								picker:action("confirm")
-							end
-						end,
-					},
-					win = {
-						list = {
-							keys = {
-								["<c-p>"] = false,
-								["<c-k>"] = false,
-								["<c-j>"] = false,
-								["<c-b>"] = false,
-								["l"] = false,
-								["h"] = false,
-								["<S-l>"] = false,
-								["<cr>"] = "toggle_dir",
-							},
-						},
-					},
-					layout = {
-						auto_hide = { "input" },
-					},
-				},
-			},
-		},
+		{ "<C-p>", "<cmd>lua Snacks.picker.files()<cr>", desc = "Find Files (Root Dir)" },
+		{ "<leader><space>", false },
 	},
 }
