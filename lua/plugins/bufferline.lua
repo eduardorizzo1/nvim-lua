4f9c03d35827403a0a226b3cf778d64c8ed3cc56
Eduardo Rizzo <eduardorizzo1@outlook.com>
2025-05-15 13:48:43 +1000

    refact bufferline configs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,27 +2,7 @@ return {
 	"akinsho/bufferline.nvim",
 	opts = {
 		options = {
-			show_tab_indicators = true,
-			always_show_bufferline = true,
-			show_close_icon = true,
-			show_buffer_close_icons = false,
-			indicator = {
-				icon = "",
-				style = "icon",
-			},
-			separator_style = "slant", -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' }
-			offsets = {
-				{
-					filetype = "NvimTree",
-					-- highlight = "Directory",
-					padding = 0,
-					separator = false,
-					text = function()
-						return " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-					end,
-					text_align = "left",
-				},
-			},
+			separator_style = "slant",
 		},
 	},
 }
