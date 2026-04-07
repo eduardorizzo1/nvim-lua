23173c2fcb5ab95a24b48b7ac8ecd2e9a12cbb81
Eduardo Rizzo <eduardorizzo1@outlook.com>
2026-04-08 07:48:58 +1000

    add test mini icons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,14 @@
 return {
-	"nvim-mini/mini.icons",
-	lazy = false,
-	opts = {
-		extension = {
-			["spec.ts"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
-			["spec.js"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
-			["test.ts"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
-			["test.js"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
+	{
+		"nvim-mini/mini.icons",
+		lazy = false,
+		opts = {
+			extension = {
+				["spec.ts"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
+				["spec.js"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
+				["test.ts"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
+				["test.js"] = { glyph = "󰂖", hl = "MiniIconsCyan" },
+			},
 		},
 	},
 }
